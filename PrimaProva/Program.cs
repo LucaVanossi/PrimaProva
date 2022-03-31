1411013987e54dd9805909421a07baab607fd050
LucaVanossi <vanossi.luca13@gmail.com>
2022-03-31 12:09:00 +0200

    Add project files.

---
+++ PrimaProva/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Aggiungi un nome");
+
+var testo = Console.ReadLine();
+
+Console.WriteLine(testo);
